733d62dae011a1387402f64e1741da838062e922
Neha Gawade <gawade.neha@gmail.com>
2016-03-07 23:37:04 -0800

    Assignment 3

--- Chapter4/homework3.js
+++ Chapter4/homework3.js
@@ -1,165 +1,176 @@
-function exercise1(nums) {
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+// Server-side code
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+/*globals _:false*/
+function main() {
+    "use strict";
 
-    var sumSoFar = 0,
-        a = 0;
+    function exercise1(nums) {
 
-    //using forEach loop
+        var sumSoFar = 0,
+            a = 0;
 
-    nums.forEach(function(value) {
-        sumSoFar = sumSoFar + value;
-    });
-    a = sumSoFar / nums.length;
-    return a;
-}
+        //using forEach loop
 
-function exercise2(nums) {
+        nums.forEach(function(value) {
+            sumSoFar = sumSoFar + value;
+        });
+        a = sumSoFar / nums.length;
+        return a;
+    }
 
-    var l = nums[0];
+    function exercise2(nums) {
 
-    //using forEach loop
+        var l = nums[0];
 
-    nums.forEach(function(value) {
-        if (value > l) {
-            l = value;
-        }
-    });
+        //using forEach loop
 
-    return l;
-}
+        nums.forEach(function(value) {
+            if (value > l) {
+                l = value;
+            }
+        });
 
-
-function exercise3(nums) {
-
-    var l = false;
-
-    //using forEach loop
-
-    nums.forEach(function(value) {
-        if (value % 2 === 0) {
-            l = true;
-            return l;
-        }
-    });
-
-    return l;
-}
+        return l;
+    }
 
 
-function exercise4(nums) {
+    function exercise3(nums) {
 
-    var l = true;
+        var l = false;
 
-    //using forEach loop
+        //using forEach loop
 
-    nums.forEach(function(value) {
-        if (value % 2 !== 0) {
-            l = false;
-            return l;
-        }
-    });
+        nums.forEach(function(value) {
+            if (value % 2 === 0) {
+                l = true;
+                return l;
+            }
+        });
 
-    return l;
-}
-
-function arrayContains(array1, str) {
-    var f = false;
-    array1.forEach(function(value) {
-        if (value === str) {
-            f = true;
-            return f;
-        }
-    });
-    return f;
-}
+        return l;
+    }
 
 
-function arrayContainsTwo(array1, str) {
-    var f = false,
-        count = 0;
-    array1.forEach(function(value) {
-        if (value === str) {
-            count = count + 1;
-            if (count === 2) {
+    function exercise4(nums) {
+
+        var l = true;
+
+        //using forEach loop
+
+        nums.forEach(function(value) {
+            if (value % 2 !== 0) {
+                l = false;
+                return l;
+            }
+        });
+
+        return l;
+    }
+
+    function arrayContains(array1, str) {
+        var f = false;
+        array1.forEach(function(value) {
+            if (value === str) {
                 f = true;
                 return f;
             }
-        }
-    });
-    return f;
-}
+        });
+        return f;
+    }
 
-function arrayContainsThree(array1, str) {
-    var f = false,
-        count = 0;
-    array1.forEach(function(value) {
-        if (value === str) {
-            count = count + 1;
-            if (count === 3) {
-                f = true;
-                return f;
+
+    function arrayContainsTwo(array1, str) {
+        var f = false,
+            count = 0;
+        array1.forEach(function(value) {
+            if (value === str) {
+                count = count + 1;
+                if (count === 2) {
+                    f = true;
+                    return f;
+                }
             }
-        }
-    });
-    return f;
-}
+        });
+        return f;
+    }
 
-
-function arrayContainsNTimes(array1, str, n) {
-    var f = false,
-        count = 0;
-    array1.forEach(function(value) {
-        if (value === str) {
-            count = count + 1;
-            if (count === n) {
-                f = true;
-                return f;
+    function arrayContainsThree(array1, str) {
+        var f = false,
+            count = 0;
+        array1.forEach(function(value) {
+            if (value === str) {
+                count = count + 1;
+                if (count === 3) {
+                    f = true;
+                    return f;
+                }
             }
-        }
-    });
-    return f;
+        });
+        return f;
+    }
+
+
+    function arrayContainsNTimes(array1, str, n) {
+        var f = false,
+            count = 0;
+        array1.forEach(function(value) {
+            if (value === str) {
+                count = count + 1;
+                if (count === n) {
+                    f = true;
+                    return f;
+                }
+            }
+        });
+        return f;
+    }
+
+    function exercise2under(nums) {
+
+        var l;
+
+        l = _.max(nums);
+
+        return l;
+    }
+
+
+    function exercise3under(nums) {
+
+        var l;
+
+        l = _.some(nums, function(num) {
+            return num % 2 === 0;
+        });
+
+        return l;
+    }
+
+
+    function exercise4under(nums) {
+        var l;
+
+        l = _.every(nums, function(num) {
+            return num % 2 === 0;
+        });
+
+        return l;
+
+    }
+
+    document.getElementById("1").innerHTML = exercise1([1, 2, 3, 4]);
+    document.getElementById("2").innerHTML = exercise2([1, 2, 3, 4]);
+    document.getElementById("3").innerHTML = exercise3([1, 2, 3, 4]);
+    document.getElementById("4").innerHTML = exercise4([1, 2, 3, 4]);
+    document.getElementById("5").innerHTML = arrayContains(["hello", "world"], "hello");
+    document.getElementById("6").innerHTML = arrayContainsTwo(["a", "b", "c", "d"], "a");
+    document.getElementById("7").innerHTML = arrayContainsThree(["a", "b", "c", "d", "a", "a"], "a");
+    document.getElementById("8").innerHTML = arrayContainsNTimes(["a", "b", "c", "d"], "a", 1);
+    document.getElementById("9").innerHTML = exercise2under([1, 2, 3, 4]);
+    document.getElementById("10").innerHTML = exercise3under([1, 2, 3, 4]);
+    document.getElementById("11").innerHTML = exercise4under([4, 2, 6, 8]);
 }
 
-function exercise2under(nums) {
-
-    var l;
-
-    l = _.max(nums);
-
-    return l;
-}
-
-
-function exercise3under(nums) {
-
-    var l;
-
-    l = _.some(nums, function(num) {
-        return num % 2 === 0;
-    });
-
-    return l;
-}
-
-
-function exercise4under(nums) {
-    var l;
-
-    l = _.every(nums, function(num) {
-        return num % 2 === 0;
-    });
-
-    return l;
-
-}
-
-document.getElementById("1").innerHTML = exercise1([1, 2, 3, 4]);
-document.getElementById("2").innerHTML = exercise2([1, 2, 3, 4]);
-document.getElementById("3").innerHTML = exercise3([1, 2, 3, 4]);
-document.getElementById("4").innerHTML = exercise4([1, 2, 3, 4]);
-document.getElementById("5").innerHTML = arrayContains(["hello", "world"], "hello");
-document.getElementById("6").innerHTML = arrayContainsTwo(["a", "b", "c", "d"], "a");
-document.getElementById("7").innerHTML = arrayContainsThree(["a", "b", "c", "d", "a", "a"], "a");
-document.getElementById("8").innerHTML = arrayContainsNTimes(["a", "b", "c", "d"], "a", 1);
-document.getElementById("9").innerHTML = exercise2under([1, 2, 3, 4]);
-document.getElementById("10").innerHTML = exercise3under([1, 2, 3, 4]);
-document.getElementById("11").innerHTML = exercise4under([4, 2, 6, 8]);
+main();
