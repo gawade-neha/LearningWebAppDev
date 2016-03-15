79dee2f8f74325fa356a9dd9f50478e0875e3ef7
Neha Gawade <gawade.neha@gmail.com>
2016-03-14 23:21:19 -0700

    Assignment 4

---
+++ Chapter5/Assignment 4/app.js
@@ -0,0 +1,69 @@
+var main = function() {
+    "use strict";
+    var addHtml;
+
+    function populateHtml() {
+        $.getJSON("http://localhost:3004/actors", function(getData) {
+
+            getData.forEach(function(getObject) {
+                if (getObject.starred === true) {
+                    addHtml = "<li class ='mdl-list__item'>" + "<span class ='mdl-list__item-primary-content'>" + "<i class='material-icons mdl-list__item-avatar'>person</i>" +
+                        "<span>" + getObject.name + "</span>" + "</span>" + "<a class='mdl-list__item-secondary-action'><i id =" + getObject.id + " class='material-icons' >star</i></a>" + "</li>";
+                    $(addHtml).appendTo('ul.mdl-list');
+                } else {
+                    addHtml = "<li class ='mdl-list__item'>" + "<span class ='mdl-list__item-primary-content'>" + "<i class='material-icons mdl-list__item-avatar'>person</i>" +
+                        "<span>" + getObject.name + "</span>" + "</span>" + "<a class='mdl-list__item-secondary-action'><i id =" + getObject.id + " class='material-icons'>star_border</i></a>" + "</li>";
+                    $(addHtml).appendTo('ul.mdl-list');
+                }
+
+            });
+        });
+    }
+   
+    $("button").on("click", function() {
+        $.post("http://localhost:3004/actors", {
+            name: $("#sample1").val(),
+            starred: false
+        });
+        $("ul").empty();
+        populateHtml();
+    });
+ populateHtml();
+    $(document).bind("click", ".mdl-list__item-secondary-action .material-icons", function (eventCall) {
+            var icon = $(event.target);
+            var id = icon.attr("id");
+            $.getJSON("http://localhost:3004/actors", function(data) {
+                data.forEach(function(actor) {
+                    if (id === JSON.stringify(actor.id)) {
+                      if (JSON.stringify(actor.starred) === "true") {
+                          $(icon).replaceWith("<i id=" +JSON.stringify(actor.id) + " class='material-icons'>star_border</i>");
+                            $.ajax({
+                                type: 'PUT',
+                                contentType: 'application/json',
+                                url: 'http://localhost:3004/actors/' + id,
+                                data: JSON.stringify({
+                                      name: actor.name,
+                                      starred: false
+                                })
+                            });
+                        }
+                        else {
+                            $(icon).replaceWith("<i id=" + JSON.stringify(actor.id) + " class='material-icons'>star</i>");
+                            $.ajax({
+                                type: 'PUT',
+                                contentType: 'application/json',
+                                url: 'http://localhost:3004/actors/' + id,
+                                data: JSON.stringify({
+                                      name: actor.name,
+                                      starred: true
+                                })
+                            });
+                        }
+                    }
+                });
+            });
+        }
+
+    );
+};
+$(document).ready(main);
