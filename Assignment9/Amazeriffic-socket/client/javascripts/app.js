2d26a825fcf9e7e5f9fd2ccf473e408defd6b146
Neha Gawade <gawade.neha@gmail.com>
2016-05-02 18:07:27 -0700

    final commit

--- Assignment9/Amazeriffic-socket/client/javascripts/app.js
+++ Assignment9/Amazeriffic-socket/client/javascripts/app.js
@@ -1,14 +1,14 @@
 /*globals ko*/
 /*globals $*/
 //creating todo object for newest and oldest tab
-
 //creating a todo object for tags tab
-var Tab = function (name, selected) {
+var Tab = function(name, selected) {
     this.name = name;
-    this.current = ko.computed(function () {
+    this.current = ko.computed(function() {
         return this === selected();
     }, this);
 };
+
 function ToDo(data) {
     this.description = ko.observable(data.description);
     this.tags = ko.observableArray(data.tags);
@@ -17,7 +17,7 @@ function ToDo(data) {
 function AppViewModel() {
     var self = this;
 
-    self.currentTab = ko.observable();//will store current tab
+    self.currentTab = ko.observable(); //will store current tab
 
     //all possible tabs array
     self.tabs = ko.observableArray([
@@ -26,7 +26,7 @@ function AppViewModel() {
         new Tab('tags', self.currentTab),
         new Tab('add', self.currentTab)
     ]);
-    
+
     //inialize to the first tab
     self.currentTab(self.tabs()[0]);
 
@@ -40,45 +40,55 @@ function AppViewModel() {
     function TagData() {
         var tags = [];
 
-        self.todos().forEach(function (toDo) {
-            toDo.tags().forEach(function (tag) {
+        self.todos().forEach(function(toDo) {
+            toDo.tags().forEach(function(tag) {
                 if (tags.indexOf(tag) === -1) {
                     tags.push(tag);
                 }
             });
         });
 
-        var tagObjects = tags.map(function (tag) {
+        var tagObjects = tags.map(function(tag) {
             var toDosWithTag = [];
 
-            self.todos().forEach(function (toDo) {
+            self.todos().forEach(function(toDo) {
                 if (toDo.tags.indexOf(tag) !== -1) {
                     toDosWithTag.push(toDo.description);
                 }
             });
 
-            return { "name": tag, "toDos": toDosWithTag };
+            return {
+                "name": tag,
+                "toDos": toDosWithTag
+            };
         });
         self.TagsTodo(tagObjects);
     }
 
     //interacts with server and returns todo objects
-    $.getJSON("/todos.json", function (result) {
-        var Todos = $.map(result, function (item) { return new ToDo(item); });
+    $.getJSON("/todos.json", function(result) {
+        var Todos = $.map(result, function(item) {
+            return new ToDo(item);
+        });
         self.todos(Todos);
         TagData();
     });
 
     //adds new todo
-    self.addToDo = function () {
+    self.addToDo = function() {
         var description = self.newTodo_description,
             tags = self.newTodo_tags,
             split_tags = tags().split(','),
-            newToDo = { "description": description, "tags": split_tags };
+            newToDo = {
+                "description": description,
+                "tags": split_tags
+            };
 
         if (description() !== "" && tags() !== "") {
-            $.post("/todos", newToDo, function (result) {
-                var Todos = $.map(result, function (item) { return new ToDo(item); });
+            $.post("/todos", newToDo, function(result) {
+                var Todos = $.map(result, function(item) {
+                    return new ToDo(item);
+                });
                 self.todos(Todos);
                 TagData();
             });
@@ -90,4 +100,3 @@ function AppViewModel() {
 }
 
 ko.applyBindings(new AppViewModel());
-
